8ddad9ea40b3e3bf5b5bc496ac89f54596d3123c
Lin-H <lh04667649@qq.com>
2021-03-23 00:56:39 +0800

    feat 增加部分cpu指令

--- readme.md
+++ readme.md
@@ -3,3 +3,7 @@
 # chip8 模拟器 [![Build Status](https://travis-ci.com/Lin-H/chip8.svg?branch=master)](https://travis-ci.com/Lin-H/chip8)
 
 使用sdl2来做跨平台，由于在windows上编译sdl2比较麻烦（需要安装CMake），所以需要在开发的时候解压`SDL2-devel-2.0.9-VC.zip`文件到`target/debug/deps/`目录下
+
+## 参考资料
+
+[CHIP-8 Classic Manual](https://storage.googleapis.com/wzukusers/user-34724694/documents/5c83d6a5aec8eZ0cT194/CHIP-8%20Classic%20Manual%20Rev%201.3.pdf)
--- src/cpu.rs
+++ src/cpu.rs
@@ -1,38 +1,92 @@
 
+use std::usize;
+
 use super::memory::Memory;
 
 pub struct CPU {
     speed: u16, // 500Hz
-    V: [u8; 16], // 16 8bit register
-    I: u16,  // 16bit address register
+    v: [u8; 16], // 16 8bit register
+    i: u16,  // 16bit address register
+    stack: Vec<u16>,
     memory: Memory,
-    pointer: usize
+    /* program counter */
+    pc: usize
 }
 
 impl CPU {
     pub fn new(memory: Memory) -> CPU {
         return CPU {
             speed: 500,
-            V: [0; 16],
-            I: 0,
+            v: [0; 16],
+            i: 0,
             memory,
-            pointer: 0x200
+            stack: Vec::new(),
+            pc: 0x200
         }
     }
     pub fn run(&mut self) {
-        let op = self.getOP();
-        self.exec(op)
+        let op = self.get_op();
+        self.exec(op);
+        self.next();
     }
     fn exec(&mut self, op: u16) {
         let op0 = (op & 0xf000) >> 12;
         let op1 = (op & 0x0f00) >> 8;
         let op2 = (op & 0x00f0) >> 4;
         let op3 = op & 0x000f;
-        println!("{:?}, {:?}, {:?}, {:?}", op0, op1, op2, op3)
+        // let op = ;
+        println!("{:?}, {:?}, {:?}, {:?}", op0, op1, op2, op3);
+        match (op0, op1, op2, op3) {
+            (0, 0, 0, 0) => println!("No Operation"),
+            (0, 0, 0xe, 0) => {
+                println!("Clear the Screen");
+            },
+            (0, 0, 0xe, 0xe) => println!("Return from Subroutine"),
+            // Jump to location MMM.
+            (1, m1, m2, m3) => {
+                let address = (m1 << 8) + (m2 << 4) + m3;
+                self.pc = address as usize;
+            },
+            // Call Subroutine.
+            (2, m1, m2, m3) => {
+                let address = (m1 << 8) + (m2 << 4) + m3;
+                self.stack.push(self.pc as u16);
+                self.pc = address as usize;
+                let op = self.get_op();
+                self.exec(op);
+            },
+            // Skip next Instruction if VX=KK
+            (3, x, k1, k2) => {
+                let kk = (k1 << 4 + k2) as u8;
+                if self.v[x as usize] == kk {
+                    self.next();
+                }
+            },
+            // Skip next Instruction if VX≠KK.
+            (4, x, k1, k2) => {
+                let kk = (k1 << 4 + k2) as u8;
+                if self.v[x as usize] != kk {
+                    self.next();
+                }
+            },
+            // Skip next Instruction if VX=VY.
+            (5, x, y, 0) => {
+                if self.v[x as usize] == self.v[y as usize] {
+                    self.next();
+                }
+            },
+            // Assign Hex value KK to Register VX
+            (6, x, k1, k2) => {
+                self.v[x as usize] = (k1 << 4 + k2) as u8;
+            },
+            _ => println!("{:?}{:?}{:?}{:?} not covered", op0, op1, op2, op3)
+        }
     }
-    fn getOP(&mut self) -> u16 { // 获取当前CPU指令 并向后2 byte移动指针
-        let op: u16 = ((self.memory.address[self.pointer] as u16) << 8) + self.memory.address[self.pointer + 1] as u16;
-        self.pointer += 2;
+    fn get_op(&mut self) -> u16 { // 获取当前CPU指令 并向后2 byte移动指针
+        let op: u16 = ((self.memory.address[self.pc] as u16) << 8) + self.memory.address[self.pc + 1] as u16;
         return op;
+    }
+    fn next(&mut self) {
+        self.pc += 2;
     }
 }
